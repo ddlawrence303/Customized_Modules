ca48e81e30648d715997a5fa890e20382660e96a
Lawrence Lin <ddlawrence303@gmail.com>
2016-02-02 12:33:22 +0800

    Update Index.php

--- Optomomodules/Contactinfo/Controller/Index/Index.php
+++ Optomomodules/Contactinfo/Controller/Index/Index.php
@@ -4,8 +4,7 @@
    *
    * [Usage]
    * 1. exec cli : php bin/magento module:enable <Vendor_Name>_<Module_Name>, such as  Optomamodules_Contactinfo, and check module status
-   * 2. override the Post action in phtml file <Vendor_Name>_<Optomo_Theme>/templates/<modify_file>.phtml,
-        which will change the post target position, such as {$root_url}/<Customized modules name>/<index entry point>/post(contactinfo/index/post) to get params
+   * 2. override the Post action in phtml file <Vendor_Name>_<Optomo_Theme>/templates/<modify_file>.phtml, which will change the post target position, such as {$root_url}/<Customized modules name>/<index entry point>/post(contactinfo/index/post) to get params
    * 3. Setting service email and contact admin users' email in Admin Interface(Magento), which is for sure about the {$sender}, {$email} and related information.
    *
    * [User Guide Reference]
