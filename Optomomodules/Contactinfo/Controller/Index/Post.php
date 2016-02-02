1d4eb2a34355b74394932f4316c1875aa0caff87
Lawrence Lin <ddlawrence303@gmail.com>
2016-02-02 12:54:03 +0800

    Update Post.php

--- Optomomodules/Contactinfo/Controller/Index/Post.php
+++ Optomomodules/Contactinfo/Controller/Index/Post.php
@@ -4,8 +4,7 @@
     *
     * [Usage]
     * 1. exec cli : php bin/magento module:enable <Vendor_Name>_<Module_Name>, such as  Optomamodules_Contactinfo, and check module status
-    * 2. override the Post action in phtml file <Vendor_Name>_<Optomo_Theme>/templates/<modify_file>.phtml,
-         which will change the post target position, such as {$root_url}/<Customized modules name>/<index entry point>/post(contactinfo/index/post) to get params
+    * 2. override the Post action in phtml file <Vendor_Name>_<Optomo_Theme>/templates/<modify_file>.phtml,which will change the post target position, such as {$root_url}/<Customized modules name>/<index entry point>/post(contactinfo/index/post) to get params
     * 3. Setting service email and contact admin users' email in Admin Interface(Magento), which is for sure about the {$sender}, {$email} and related information.
     *
     * [User Guide Reference]
@@ -19,7 +18,7 @@
     * [Create Time] 2016.02.02
    */
 
- namespace Optomamodules\Contactinfo\Controller\Index;
+namespace Optomamodules\Contactinfo\Controller\Index;
 
 class Post extends \Magento\Contact\Controller\Index
  {
