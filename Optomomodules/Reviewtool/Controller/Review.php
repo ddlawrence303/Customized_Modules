d3d327c39d97703eb3c5dac5c8bba58420a74f0b
Lawrence Lin <ddlawrence303@gmail.com>
2017-03-16 02:16:00 +0800

    add review

---
+++ Optomomodules/Reviewtool/Controller/Review.php
@@ -0,0 +1,48 @@
+<?php
+namespace Optomamodules\Reviewtool\Controller;
+
+abstract class Review extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * Customer session
+     *
+     * @var \Magento\Customer\Model\Session
+     */
+    protected $customerSession;
+
+
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+	\Magento\Framework\View\Result\PageFactory $resultPageFactory		
+    )
+    {
+        $this->customerSession = $customerSession;
+     	$this->resultPageFactory = $resultPageFactory;
+	parent::__construct($context);
+    }
+
+    /**
+     * Check customer authentication for some actions
+     *
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @return \Magento\Framework\App\ResponseInterface
+     */
+    public function dispatch(\Magento\Framework\App\RequestInterface $request)
+    {
+        if (!$this->customerSession->authenticate()) {
+            $this->_actionFlag->set('', 'no-dispatch', true);
+            if (!$this->customerSession->getBeforeUrl()) {
+                $this->customerSession->setBeforeUrl($this->_redirect->getRefererUrl());
+            }
+        }
+        return parent::dispatch($request);
+    }
+}
