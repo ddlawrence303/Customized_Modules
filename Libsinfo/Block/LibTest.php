902f1e457c632676f1eba67195a3dae1d3f943af
Lawrence Lin <ddlawrence303@gmail.com>
2017-03-16 02:06:36 +0800

    add libtest

---
+++ Libsinfo/Block/LibTest.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is block controller should be modified
+
+namespace Optomamodules\Libsinfo\Block;
+
+class LibTest extends \Magento\Framework\View\Element\Template
+{
+	public function _prepareLayout()
+	{
+		return parent::_prepareLayout();
+	}
+}
